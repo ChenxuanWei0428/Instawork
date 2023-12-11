498e24181bf433062d93238778753762907cdd81
ChenxuanWei0428 <chenxuan.wei0428@gmail.com>
2023-12-11 11:47:11 -0500

    add model test

--- member_page/tests.py
+++ member_page/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from .models import TeamMember
 
-# Create your tests here.
+
+class TeamMemberModelTest(TestCase):
+    def setUp(self):
+        # Create a test team member
+        self.team_member = TeamMember.objects.create(first_name='Chenxuan', 
+                                                     last_name="Wei", 
+                                                     role='admin', 
+                                                     email="test@gmail.com", 
+                                                     phone_number="1234567890")
+
+    def test_team_member_creation(self):
+        # Test if the team member is created correctly
+        self.assertEqual(self.team_member.first_name, 'Chenxuan')
+        self.assertEqual(self.team_member.last_name, "Wei")
+        self.assertEqual(self.team_member.email, "test@gmail.com")
+        self.assertEqual(self.team_member.phone_number, "1234567890")
+        self.assertEqual(self.team_member.role, 'admin')
+        
